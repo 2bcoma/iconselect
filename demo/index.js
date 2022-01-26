9eb7d05f5a900be6e4bec447a66c7400a551eb75
2bcoma <2bcomacat@gmail.com>
2022-01-26 23:46:01 +0100

    simplify option propagation

--- demo/index.js
+++ demo/index.js
@@ -18,8 +18,10 @@ element.style.right = '50%'
 element.style.transform = 'translate(-50%, -50%)'
 document.body.appendChild(element)
 const iconSelect = new IconSelect(null, element, {
-	revertHorizontalAxis: true,
-	revertVerticalAxis: true,
+	horizontalIconNumber: 4,
+	verticalIconNumber: 4,
+	boxOnLeft: true,
+	boxOnTop: true,
 })
 iconSelect.refresh([
 	{ iconFilePath: i0, iconValue: 0 },
@@ -32,4 +34,26 @@ iconSelect.refresh([
 	{ iconFilePath: i7, iconValue: 7 },
 	{ iconFilePath: i8, iconValue: 8 },
 	{ iconFilePath: i9, iconValue: 9 },
+	//
+	{ iconFilePath: i0, iconValue: 10 },
+	{ iconFilePath: i1, iconValue: 11 },
+	{ iconFilePath: i2, iconValue: 12 },
+	{ iconFilePath: i3, iconValue: 13 },
+	{ iconFilePath: i4, iconValue: 14 },
+	{ iconFilePath: i5, iconValue: 15 },
+	{ iconFilePath: i6, iconValue: 16 },
+	{ iconFilePath: i7, iconValue: 17 },
+	{ iconFilePath: i8, iconValue: 18 },
+	{ iconFilePath: i9, iconValue: 19 },
+	//
+	{ iconFilePath: i0, iconValue: 20 },
+	{ iconFilePath: i1, iconValue: 21 },
+	{ iconFilePath: i2, iconValue: 22 },
+	{ iconFilePath: i3, iconValue: 23 },
+	{ iconFilePath: i4, iconValue: 24 },
+	{ iconFilePath: i5, iconValue: 25 },
+	{ iconFilePath: i6, iconValue: 26 },
+	{ iconFilePath: i7, iconValue: 27 },
+	{ iconFilePath: i8, iconValue: 28 },
+	{ iconFilePath: i9, iconValue: 29 },
 ])
